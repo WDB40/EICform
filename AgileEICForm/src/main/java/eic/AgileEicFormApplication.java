92859a710d48803bc24cb2bbd90aaea10e704cb2
WDB40 <wbrown1640@gmail.com>
2019-04-09 19:43:38 -0500

    Initial Commit

---
+++ AgileEICForm/src/main/java/eic/AgileEicFormApplication.java
@@ -0,0 +1,13 @@
+package eic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileEicFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileEicFormApplication.class, args);
+	}
+
+}
